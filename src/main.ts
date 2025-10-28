313e386e179b1e1f76e96313013c15d67f9181c5
Akintayo A. Olusegun <akintayo.segun@gmail.com>
2025-10-28 10:53:06 +0100

    more than one origin in cors

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,14 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(express.json({ limit: '100mb' }));
   app.use(express.urlencoded({ limit: '100mb', extended: true }));
-  app.enableCors({
-    origin: 'http://localhost:5173', // or your frontend's URL
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-  });  
+    app.enableCors({
+      origin: [
+        'http://localhost:5173',
+        'https://haircreed-frontend.onrender.com'
+      ],
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+      credentials: true,
+    });
   await app.listen(3000);
 }
 bootstrap();
